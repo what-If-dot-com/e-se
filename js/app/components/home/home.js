b6d91754b35695139976bd00e801a663b0b413e2
hsalmeida <hsalmeida@gmail.com>
2016-07-07 09:53:40 -0300

    retirada temporaria do bg

--- js/app/components/home/home.js
+++ js/app/components/home/home.js
@@ -3,8 +3,6 @@ angular.module('e-se')
 
         function ($scope, $rootScope, $state) {
             $rootScope.bodybg = {
-                "background": "url('img/bg.jpg') no-repeat center center fixed",
-                "background-size": "cover",
                 "width": "100%",
                 "overflow-y": "scroll"
             };
