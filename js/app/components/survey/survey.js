87a7f83c238115d24d8ad3eb03f6611610d3be33
hsalmeida <hsalmeida@gmail.com>
2016-07-08 18:26:23 -0300

    ajustes

--- js/app/components/survey/survey.js
+++ js/app/components/survey/survey.js
@@ -1,9 +1,30 @@
 angular.module('e-se')
     .controller('SurveyIndexController', ['$scope', '$rootScope', '$state',
-
-        function ($scope, $rootScope, $state) {
+        '$modal',
+        function ($scope, $rootScope, $state, $modal) {
             $rootScope.bodybg = {
                 "width": "100%",
                 "overflow-y": "scroll"
             };
+
+            $scope.newSurvey = function () {
+                $modal
+                    .open({
+                        templateUrl: 'views/survey/new-survey.html',
+                        controller: 'NewSurveyController',
+                        resolve: {
+                            surveyScope: function () {
+                                return $scope
+                            }
+                        }
+                    }).result.then(function () {
+                    }, function () {
+                    });
+            };
+
         }]);
+
+angular.module('e-se')
+    .controller('NewSurveyController', ['$scope', 'surveyScope', function ($scope, surveyScope) {
+
+    }]);
