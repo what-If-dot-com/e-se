2a62c69b3999307e203c3202024de57c8ab6c92e
hsalmeida <hsalmeida@gmail.com>
2016-07-11 08:54:33 -0300

    agora com banco :)

--- js/app/components/survey/survey.js
+++ js/app/components/survey/survey.js
@@ -1,10 +1,38 @@
 angular.module('e-se')
     .controller('SurveyIndexController', ['$scope', '$rootScope', '$state',
-        '$modal',
-        function ($scope, $rootScope, $state, $modal) {
+        '$modal', 'Pesquisas',
+        function ($scope, $rootScope, $state, $modal, Pesquisas) {
             $rootScope.bodybg = {
                 "width": "100%",
                 "overflow-y": "scroll"
+            };
+
+            $scope.pesquisas = [];
+            $scope.pesquisasPassadas = [];
+
+            $scope.initIndex = function () {
+                var q = {
+                    "ativa": true
+                };
+                Pesquisas.query(q).then(function (pesquisasDB) {
+                    if (pesquisasDB[0]) {
+                        angular.forEach(pesquisasDB, function (pesquisa, pesquisaIndex) {
+                            pesquisa.criacao = new Date(pesquisa.criacao);
+                        });
+                        $scope.pesquisas = pesquisasDB;
+                    }
+                });
+                var qp = {
+                    "ativa": false
+                };
+                Pesquisas.query(qp).then(function (pesquisasPassadasDB) {
+                    if (pesquisasPassadasDB[0]) {
+                        angular.forEach(pesquisasPassadasDB, function (pesquisaPassada, pesquisaIndex) {
+                            pesquisaPassada.criacao = new Date(pesquisaPassada.criacao);
+                        });
+                        $scope.pesquisasPassadas = pesquisasPassadasDB;
+                    }
+                });
             };
 
             $scope.newSurvey = function () {
--- js/app/e-se.services.js
+++ js/app/e-se.services.js
@@ -1,4 +1,7 @@
 angular.module("e-se")
     .factory('Usuarios',function($mongolabResourceHttp){
         return $mongolabResourceHttp('usuarios');
+    })
+    .factory('Pesquisas',function($mongolabResourceHttp){
+        return $mongolabResourceHttp('pesquisas');
     });
